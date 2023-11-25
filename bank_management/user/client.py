ab97cca598d1bc64223286b48a568960d58a4c77
pengyuyang-315 <572858610@qq.com>
2023-11-24 23:25:44 -0800

    user module upgrade

---
+++ bank_management/user/client.py
@@ -0,0 +1,34 @@
+class client:
+    total_number = 0
+    def __init__(self,name,email,phoneNumber,balance,):
+        self.name = name
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self._balance = balance
+    
+    def save_money(self,amount):
+        self._balance += amount
+        print("Successfully saved!")
+    
+    def withdraw_money(self,amount):
+        if self._balance<amount:
+            print("Not enough balance")
+        else:
+            self._balance-=amount
+            print("Successfully withdrew")
+
+    def transfer(self,amount,receiver):
+        if(self._balance<amount):
+            print("Not enough money")
+        else:
+            receiver.save_money(amount)
+            self.withdraw_money(amount)
+
+    def show_information(self):
+        return self._balance
+
+c1 = client("Annie","xx.com","12345",1000)
+c2 = client("Billy","yy.com","543221",1000)
+c1.transfer(500,c2)
+print(c1.show_information())
+print(c2.show_information())
