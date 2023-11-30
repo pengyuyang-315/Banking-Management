8ae245e8a4586f7b4d3ca40b310b197ed2a94408
pengyuyang-315 <572858610@qq.com>
2023-11-30 11:47:32 -0800

    new client open account

--- bank_management/start.py
+++ bank_management/start.py
@@ -1,29 +1,37 @@
 from user.client import client
+from user.client import new_user_registration
+
+
+# initialize clients
+client1 = client("Billy","billy@gmail.com",123456,1000,123456)
+client2 = client("Lilly","Lilly@gmail.com",123456,2000,123456)
+client3 = client("Sally","Sally@gmail.com",123456,3000,123456)
+
+clients_dict = {
+    client1.name: client1,
+    client2.name: client2,
+    client3.name: client3
+}
 
 def main():
     print("Welcome to AZYY Bank")
-    role = int(input("Plz choose your role:1. I'm new to open accoun,2. Existing client,3. Admin,4. Investment Manager\n"))
+    role = int(input("Plz choose your role:\n1. I'm new to open account,2. Existing client,3. Admin,4. Investment Manager,5. Quit\n"))
 
     if role == 1:
-        new_user_registration()
+        c_new = new_user_registration()
+        clients_dict[c_new.name] = c_new
+        main()
     elif role == 2:
         existing_user_login()
     elif role == 3:
         admin_login()
     elif role == 4:
         investment_manager_login()
+    elif role == 5:
+        print("Bye!")
+        return 
     else:
         print("Invalid, run it again")
-
-def new_user_registration():
-    name = input("Plz input your name: ")
-    phone = input("Plz input your phone number: ")
-    initial_balance = float(input("Plz save your money: "))
-    email = input("Plz input your email: ")
-    password = input("plz input your password: ")
-
-    c_new = client(name,email,phone,initial_balance,password)
-    
 
 def existing_user_login():
     # 在这里编写老用户登录的代码
@@ -39,3 +47,6 @@ def investment_manager_login():
 
 if __name__ == "__main__":
     main()
+    keys_list = list(clients_dict.keys())
+    print(keys_list)
+    
